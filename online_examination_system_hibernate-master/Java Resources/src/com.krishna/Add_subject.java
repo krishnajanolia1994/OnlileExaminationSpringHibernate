45450932eb5308d00149ca16590ad4352e6e49ef
krishna janoliya <krishna.janolia@gmail.com>
2018-06-12 17:39:05 +0530

    Update Add_subject.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Add_subject.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Add_subject.java
@@ -15,72 +15,49 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
-
-public class Add_subject extends HttpServlet {
-	
+@Entity
+@Chachable
+@Chache(Usage=ChacheConcarancyStratagy.READ_WRITE)
+public class Add_subject extends HttpServlet
+{
+	String subject;
+	@ManyToOne
+	Teacher_signup ts=new Teacher_signup ();
+	@OneToMany(MappedBy="")
+	ArrayList<Add_test> test_list =new ArrayList<>();
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
 	{
-		Connection con=null;
-		Statement st=null;
 		HttpSession se=(HttpSession) req.getSession();
+		Configuration con=new Configuration().Configure().addAnnotatedClass(Teacher_signup.class);
+		ServiceRegistry sr=new ServiceRegistryBuilder().addApliedSetings(con.getProperties()).BuildServiceRegistry();
+		SessioFactory sf=con.BuildSessionFactory(sr);
+		Session hibernet_session=sf.openSeeion();
+		Quary q=hibernet_session.createQuary("select teacher_id form Teacher_signup where password = : psaa");
+		HttpSession session=req.getSession();
+		String pass=(String)session.getAtribute("teacher_password");
+		q.setParameter("pass",pass);
+		Integer id=(Integer) q.uniqueResult();
+		Transection tx=hibernet_session.biginTransection();
+		Teacher_signup ts=hibernet_session.get(Teacher_signup,id);
+		ArraList<Add_subject> suject_list=ts.subject_list;
+		String subject=req.getParameter("subject_name");
+		if(subject_list.contains(subject_name))
+		{
+			se.setAttribute("subject_exist_error","given subject is Alraidy Exist ");
+			res.sendRedirect("add_subjectT.jsp");
+		}
 		
-		try
+		else
 		{
-			Class.forName("com.mysql.jdbc.Driver");
-			con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/online_examination_syatem","root","");
-			st =(Statement) con.createStatement();
-			String subject=req.getParameter("subject_name");
-			String table=se.getAttribute("email")+"_subject_main_table";
-			ResultSet rs =st.executeQuery("select Subject_Name from "+table+" where Subject_Name = '"+subject+"'");
-			if(rs.next())
-			{
-				se.setAttribute("subject_exist_error","given subject is Alraidy Exist ");
-				res.sendRedirect("add_subjectT.jsp");
-			}
-			else
-			{
-				PreparedStatement pst=(PreparedStatement) con.prepareStatement("insert into "
-			+se.getAttribute("email")+"_subject_main_table (Subject_Name) values (?)");
-				pst.setString(1,subject);
-				pst.executeUpdate();
-				String create_table="CREATE TABLE "+se.getAttribute("email")+subject+""
-						+ "( Test_id int AUTO_INCREMENT , Test_Name varchar(20),Number_Question int,hour int,"
-						+ " minute int , PRIMARY KEY(Test_id))";
-				st.execute(create_table);
-				se.setAttribute("subject",subject);
-				pst.close();
-				res.sendRedirect("add_testT.jsp");
-				
-				
-			}
+			subject_list.add(subject_name);
+			session.setAttribute("subject",subject_name);
 			
-		} 
-		catch (ClassNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		catch (SQLException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		finally 
-		{
-			try {
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				st.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		tx.comit();
+		res.sendRedirect("add_testT.jsp");
+			
+		
 		
 	}
 
