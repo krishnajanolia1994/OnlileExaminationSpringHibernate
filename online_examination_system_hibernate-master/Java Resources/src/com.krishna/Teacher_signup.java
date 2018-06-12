49e686c0077fd1c7aa1201e17c74725e5efae0a5
krishna janoliya <krishna.janolia@gmail.com>
2018-06-12 15:50:09 +0530

    Update Teacher_signup.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Teacher_signup.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Teacher_signup.java
@@ -14,7 +14,14 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 
 
-public class Teacher_signup extends HttpServlet {
+public class Teacher_signup extends HttpServlet 
+{
+	@id
+	int teacher_id;
+	String first_name;
+	String last_name;
+	String email_eddress;
+	String password;
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
 	{
@@ -22,42 +29,21 @@ public class Teacher_signup extends HttpServlet {
 
 		String login_table="teacher_login_table";
 		int redirect;
-		try {
-			redirect=service.sevicess(req,res,login_table);
-			if(redirect==1)
-			{
-				String email=req.getParameter("Email_Address");
-				String temp1,temp2;
-				int index=email.indexOf("@");
-				temp1=email.substring(0, index);
-				temp2=email.substring(index+1, email.length()-1);
-				email=temp1+temp2;
-				
-				String table=email+"_subject_main_table";
-				String str="CREATE TABLE  "+table+"(  Sub_id int AUTO_INCREMENT , "
-						+ "Subject_Name varchar(20),PRIMARY KEY(Sub_id))";
-				Class.forName("com.mysql.jdbc.Driver");
-				Connection con=null;
-			    con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/online_examination_syatem", "root", "");
-			    Statement st=(Statement) con.createStatement();
-			    st.execute(str);
-			    con.close();
-			    HttpSession se=req.getSession();
-			    se.setAttribute("email", email);
-				res.sendRedirect("display_teacher.jsp");
-				
-			}
-			else
-			{
-				res.sendRedirect("sign-up-login-form/index.html");
-			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+		redirect=service.sevicess(req,res,login_table);
+		if(redirect==1)
+		{
+			String pass=req.getParameter("password");
+		    HttpSession se=req.getSession();
+		    se.setAttribute("teacher_password", pass);
+			res.sendRedirect("display_teacher.jsp");
+
 		}
+		else
+		{
+			res.sendRedirect("sign-up-login-form/index.html");
+		}
+		
 		
 	}
 
