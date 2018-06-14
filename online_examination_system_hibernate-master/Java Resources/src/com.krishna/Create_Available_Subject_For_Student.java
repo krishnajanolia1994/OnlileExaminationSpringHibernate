be57f401fa2e125ede6e100c6f02106055ac3071
krishna janoliya <krishna.janolia@gmail.com>
2018-06-14 18:06:23 +0530

    Update Create_Available_Subject_For_Student.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Create_Available_Subject_For_Student.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Create_Available_Subject_For_Student.java
@@ -20,34 +20,28 @@ public class Create_Available_Subject_For_Student extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
 	{
 		HttpSession se=req.getSession() ;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con1=null;
-			try {
-				con1 = (Connection) DriverManager.getConnection("jdbc:mysql:"
-						+ "//localhost:3306/online_examination_syatem","root","");
-				Statement st=(Statement) con1.createStatement();
-				String table=(String) se.getAttribute("Email_Eddress_Table")+"_subject_main_table";
-				ResultSet rs=st.executeQuery("select Subject_Name from  "+table);
-				int i=0;
-				while(rs.next())
-				{
-					
-					se.setAttribute(i+"Avalable_Subject", rs.getString("Subject_Name"));
-					
-					i++;
-				}
-				se.setAttribute("avable_subject_number", i);
-				res.sendRedirect("available_subject_for_student.jsp");
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		String teacher_email=(String)se.getAttribute("teacher_email");
+		Configuration con=new Configuration().Configure().addAnnotatedClass(Add_subject.class).addAnnotatedClass(Add_test.class).addAnnotatedClass(Id.class);
+		ServiceRegistry sr=new ServiceRegistryBuilder().addApliedSetings(con.getProperties()).BuildServiceRegistry();
+		SessioFactory sf=con.BuildSessionFactory(sr);
+		Session hibernet_session=sf.openSeeion();
+		Quary q=hibernet_session.createQuary("select teacher_id from Teacher_signup where teacher_email = : add");
+		q.setParameter("add"teacher_email);
+		Long id=q.uniqueResult();
+		Teacher_signup teacher_signup=hibernet_session.get(Teacher_signup.class,id);
+		ArrayList<Add_subject> subject_list =teacher_signpu.subject_list;
+		int i=0;
+		for(Add_subject subject_obj : subject_list)
+		while(rs.next())
+		{
+
+			se.setAttribute(i+"Avalable_Subject", subject_obj);
+
+			i++;
 		}
+		se.setAttribute("avable_subject_number", i);
+		res.sendRedirect("available_subject_for_student.jsp");
+			
 		
 	}
 
