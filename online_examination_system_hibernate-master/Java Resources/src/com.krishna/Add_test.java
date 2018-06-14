a9c2472a95c279b84045913f1e55901b51ec875c
krishna janoliya <krishna.janolia@gmail.com>
2018-06-14 17:11:51 +0530

    Update Add_test.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Add_test.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Add_test.java
@@ -25,7 +25,7 @@ public class Add_test extends HttpServlet
 	@Id
 	int test_id
 	String test_name;
-	int Number_question;
+	int number_question;
 	int num_hour ;
 	int num_min;
 	@ManyToOne
@@ -38,39 +38,37 @@ public class Add_test extends HttpServlet
 		
 		HttpSession se=req.getSession();
 		String test_name=req.getParameter("test_name");
-		int Number_question =Integer.parseInt(req.getParameter("Number_question"));
+		int number_question =Integer.parseInt(req.getParameter("Number_question"));
 		int num_hour=Integer.parseInt(req.getParameter("hours"));
 		int num_min=Integer.parseInt(req.getParameter("minute"));
 		String Subject=(String) se.getAttribute("subject");
 		pr.println(Subject);
-		Configuration con=new Configuration().Configure().addAnnotatedClass(Add_subject.class).addAnnotatedClass(Add_test.class);
+		Configuration con=new Configuration().Configure().addAnnotatedClass(Add_subject.class).addAnnotatedClass(Add_test.class).addAnnotatedClass(Id.class);
 		ServiceRegistry sr=new ServiceRegistryBuilder().addApliedSetings(con.getProperties()).BuildServiceRegistry();
 		SessioFactory sf=con.BuildSessionFactory(sr);
 		Session hibernet_session=sf.openSeeion();
-		Quary q=hibernet_session.createQuary("select subject_id from Add_subject where subject =:sub");
-		q.setPrameter("sub", Subject);
-		Integer id=p.uniqueResult();
+		Long id=(Long)se.getAttribute("subject_id");
 		Transection tx=hibernet_session.biginTransection();
-		Add_subject add_sub=hibernet_session.get(Add_subject.class,id);
-		ArrayList<Add_test> test_list=add_sub.Add_test;
-		if(test_list.contais(test_name))
+		Add_subject subject_obj=hibernet_session.get(Add_subject.class,id);
+		ArrayList<Add_test> test_list=subject_obj.test_list;
+		for(Add_test test_obj:test_list)
 		{
-			se.setAttribute("test_exist_error","given test is Alraidy Exist ");
-			res.sendRedirect("add_testT.jsp");
+			if(test_obj.test_name.equals(test_name))
+				res.sendRedirect("add_testT.jsp");
 		}
-		else
-		{
-			Add_teat new_test=new Add_test();
-			new_test.test_name=test_name;
-			new_test.Number_question=Number_question;
-			new_test.num_hour=num_hour ;
-			new_test.num_min=num_min;
-			test_list.add(new_test);
-			
-		}
-		hibernet_session.save(add_sub);
+		Id id=hibernet_session.get(Id.class,1000);
+		Add_test new_test =new Add_test();
+		 new_test.test_name=test_name();
+		 new_test.number_question=number_question;
+		 new_test.num_hour=num_hour;
+		 new_test.num_min=num_min;
+		 new_test.test_id=id.test_id;
+		 id.test_id++;
+		 test_list.add(new_test);
+		 hibernet_session.save(subject_obj);
+		 hibernet_session.save(id);
 		tx.comit();
-		se.setAtribute("test_name",test_name);
+		se.setAtribute("test_id",new_test.test_id);
 		res.sendRedirect("add_question.jsp");
 		
 	}
