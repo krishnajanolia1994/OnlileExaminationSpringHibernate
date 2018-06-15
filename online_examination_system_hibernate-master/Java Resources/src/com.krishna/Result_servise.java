57777f01f2c77a2b90ec1ba318ea66aa65819b72
krishna janoliya <krishna.janolia@gmail.com>
2018-06-15 17:29:40 +0530

    Update Result_servise.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Result_servise.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Result_servise.java
@@ -13,56 +13,39 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
-public class Result_servise {
+public class Result_servise 
+{
+	String test_name;
+	int total_marks;
+	int marks_obtai;
+	@ManyToOne
+	Student_signup stud;
 	public void servise(HttpServletRequest req, HttpServletResponse res)
 	{
 		HttpSession se=req.getSession();
-		try 
-		{
-			Connection Con1=null;
-			Class.forName("com.mysql.jdbc.Driver");
-			try {
-				Con1 = (Connection) DriverManager.getConnection("jdbc:mysql:"
-						+ "//localhost:3306/online_examination_syatem","root","");
-				String table=(String) se.getAttribute("email_of_student");
-				int total_marks=(Integer) se.getAttribute("total_marks");
-				int marks_obtain=(Integer) se.getAttribute("marks_obtain");
-				String test=(String) se.getAttribute("test_table");
-
-				PreparedStatement pst=(PreparedStatement) Con1.prepareStatement("insert into  "+table+  "(  test_name , maximum_marks , marks_obtsin) values(? , ? , ? )");
-				pst.setString(1, test);
-				pst.setInt(2, total_marks);
-				pst.setInt(3, marks_obtain);
-				pst.executeUpdate();
-				pst.close();
-				try {
-					res.sendRedirect("display.jsp");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} catch (SQLException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			finally
-			{
-				try {
-					Con1.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-		} 
-		catch (ClassNotFoundException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		String std_password=(String)se.getAttribute("student_password");
+		Configuration con=new Configuration().Configure().addAnnotatedClass(Teacher_signup.class).addAnnotatedClass(Add_subject.class);
+		ServiceRegistry sr=new ServiceRegistryBuilder().addApliedSetings(con.getProperties()).BuildServiceRegistry();
+		SessioFactory sf=con.BuildSessionFactory(sr);
+		Session hibernet_session=sf.openSeeion();
+		Quary q=hibernet_session.CreateQuary("select student_id form Student_signup where password=: pass");
+		q.setParameter("pass",sts_password);
+		long id=(Long)q.uniqueResult();
+		Student_signup std_obj =hibernet_session.get(Student_signup.class,id);
+		ArrayList<Result_servise> result_list =std_obj.result_list;
+				
+		int total_marks= Integer.parseInt(se.getAttribute("total_marks"));
+		int marks_obtain= Integer.parseInt(se.getAttribute("marks_obtain"));
+		String test=(String) se.getAttribute("test_name");
+		Result_servise reslt=new Result_servise ();
+		result.test_name=test;
+		rsult.total_marks=total_marks;
+		result.marks_obtain=marks_obtain;
+		result_list.add(result);
+		Transection tx=hibernets_ession.biginTransection();
+		hibernet_session.save(std_obj);
+		tx.comit();
+		res.sendRedirect("display.jsp");
 	}
 
 }
