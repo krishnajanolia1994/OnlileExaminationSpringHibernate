27aa79e7c0ca2cedbdc3fed553c75ffeb91e0167
krishna janoliya <krishna.janolia@gmail.com>
2018-06-15 14:38:57 +0530

    Update Add_Subject_to_Session_student.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Add_Subject_to_Session_student.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Add_Subject_to_Session_student.java
@@ -11,10 +11,11 @@ import javax.servlet.http.HttpSession;
 public class Add_test_to_session_student extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		String test_name=req.getParameter("test_name_student");
+		String subject_id=req.getParameter("subject_id");
 		HttpSession session=req.getSession();
-		session.setAttribute("test_table",test_name);
-		res.sendRedirect("create_question_paper.jsp");
+		Long sub_id=Long.paseLong(subject_id);
+		session.setAttribute("subject_id",sub_id);
+		res.sendRedirect("create_test_for_student.jsp");
 	}
 
 }
