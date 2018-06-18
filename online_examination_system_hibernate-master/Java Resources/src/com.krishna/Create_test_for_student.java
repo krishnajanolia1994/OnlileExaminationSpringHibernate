09a3ba855cc5592ee9048053faab6e73c7f6c8ad
krishna janoliya <krishna.janolia@gmail.com>
2018-06-18 11:42:21 +0530

    Update Create_test_for_student.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Create_test_for_student.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Create_test_for_student.java
@@ -11,7 +11,7 @@ import org.hibernet.SessionFactory;
 import org.hibernet.cfg.Configuration;
 import org.hibernet.service.ServiceRegistry;
 import org.hibernet.service.ServiceRegistryBuilder;
-javax.persitence.Cacheable;
+import  javax.persitence.Cacheable;
 
 
 
