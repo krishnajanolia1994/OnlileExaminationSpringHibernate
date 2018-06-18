ca93bf054e658d3825bba2877c89d980622781c3
krishna janoliya <krishna.janolia@gmail.com>
2018-06-18 15:59:29 +0530

    Create AppConfig.java

---
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/AppConfig.java
@@ -0,0 +1,7 @@
+import orgspringframwork.context.annotation.CoponentScan;
+import orgspringframwork.context.annotation.Configuration;
+@Configuration
+@ComponentScan(basepackage="com.krishna")
+public class AppConfig
+{
+}
