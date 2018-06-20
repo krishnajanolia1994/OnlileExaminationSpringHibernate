c48f559ed8b0b888e254e77c9d4c5b38669ae242
krishna janoliya <krishna.janolia@gmail.com>
2018-06-20 15:30:22 +0530

    Update Student_login.java

--- online_examination_system_hibernate-master/Java Resources/src/com.krishna/Student_login.java
+++ online_examination_system_hibernate-master/Java Resources/src/com.krishna/Student_login.java
@@ -1,14 +1,15 @@
 package com.krishna;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.hibernet.Session;
+import org.hibernet.SessionFactory;
+import org.hibernet.cfg.Configuration;
+import org.hibernet.service.ServiceRegistry;
+import org.hibernet.service.ServiceRegistryBuilder;
 
 
 public class Student_login extends HttpServlet {
